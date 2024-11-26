0dd1bf2648be576e3334fbf0d52dea0e38fbe594
CHEMSI YASSER <chemsiy@3il.fr>
2024-11-26 16:48:05 +0100

    amélioration de la qualité du code de l' exercice 3 en ajoutant les commetaires la méthode KISS ET DRY

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,36 @@
-public class StockManager {
+public class exercice3 {
+
+    /**
+     * simplification de la methode en utulisant le kiss et dry
+     * @param typeOperation
+     * @param produit
+     * @param quantite
+     * @param stock
+     */
 
     // gere le stock
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
         // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
+        switch (typeOperation) {
+            case "ajout" -> ajout_du_stock(produit, stock, quantite);
+            // Retrait du stock
+            case "retrait" -> Retrait_du_stock(produit, stock, quantite);
+            // Si l'action est inconnu
+            default -> System.out.println("Opération inconnue.");
+        }
+    }
+    // methoce ajout du stock
+    public void ajout_du_stock (String produit , int stock , int quantite) {
+        stock += quantite;
             System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
+    }
+    // methode retrait du stock
+    public void  Retrait_du_stock (String produit , int stock , int quantite){
+        if (stock >= quantite) {
+            stock -= quantite;
+            System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
         } else {
-            System.out.println("Opération inconnue.");
+            System.out.println("Stock insuffisant pour le produit : " + produit);
         }
     }
 }
