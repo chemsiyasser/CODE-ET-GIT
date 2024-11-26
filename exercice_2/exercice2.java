9c3f7d856c0e7df4c7b4a10672e50c19fe104f2a
CHEMSI YASSER <chemsiy@3il.fr>
2024-11-26 16:32:55 +0100

    amélioration de la qualité du code de l' exercice 2 en ajoutant les commetaires la méthode KISS ET DRY

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,20 +1,39 @@
-public class GestionnaireNotes {
-
+public class exercice2 {
+    /**
+     * simplification du code par kiss et dry
+     * @return 
+     * @param nomEtudiant
+     * @param notes
+     */
     // Affiche note et moyennes
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
         // Affichage des notes
         System.out.println("Notes de " + nomEtudiant + ":");
+        afficherNotes(notes);
+
+        // Calcul de la somme des notes
+        calculersomme(notes);
+
+        // Affichage de la moyenne
+        afficherMoyenne(calculersomme(notes), notes);
+    }
+     
+    // methode pour  l' Affichage des notes 
+    public void afficherNotes (int[] notes){
         for (int note : notes) {
             System.out.println("- " + note);
         }
-
-        // Calcul de la somme des notes
+    }
+    // methode de Calcul de la somme des notes
+    public int calculersomme (int[] notes ) {
         int somme = 0;
-        for (int note : notes) {
-            somme += note;
+        for (int note2 : notes) {
+            somme += note2;
         }
-
-        // Affichage de la moyenne
+        return somme ;
+    }
+    // methode pour l'Affichage de la moyenne
+    public void afficherMoyenne (double somme , int[] notes){
         double moyenne = (double) somme / notes.length;
         System.out.println("Moyenne : " + moyenne);
     }
